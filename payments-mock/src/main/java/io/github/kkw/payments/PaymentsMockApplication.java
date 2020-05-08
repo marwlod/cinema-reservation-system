ce4de33e6e97eac045a34e8bf019f3d3ec523040
mardzin <martin.wlod@gmail.com>
2020-05-08 16:00:37 +0200

    External payments system mock

---
+++ payments-mock/src/main/java/io/github/kkw/payments/PaymentsMockApplication.java
@@ -0,0 +1,11 @@
+package io.github.kkw.payments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentsMockApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentsMockApplication.class, args);
+    }
+}
