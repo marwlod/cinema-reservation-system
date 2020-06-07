186e232a8ce115e10f7ae313d06d503577b65bf8
bartoszkordek <bartosz.kordek10@gmail.com>
2020-06-07 20:47:50 +0200

    ContactUs

--- front/src/components/ContactUs.jsx
+++ front/src/components/ContactUs.jsx
@@ -1,22 +1,18 @@
 import React from "react";
-
+const year = new Date().getFullYear();
 export default function Contact(props) {
 
-    function Footer() {
-        const year = new Date().getFullYear();
-        return (
-            <footer>
-                <p>Copyright ⓒ {year}</p>
-            </footer>
-        );
-    }
     return (
+
         <div className={props.className}>
+
             <h2><b>Welcome to AGH Cinema</b></h2><br/><br/>
             <b>email:</b> <i>cinema@agh.com</i><br/>
             <b>Address: </b>ul. Kawiory 21, 30-055 Kraków<br/>
-            <b>Phone: </b>tel. 655 123 456<br/>
-{/*            <ContactUsFooter />*/}
+            <b>Phone: </b>tel. 655 123 456<br/><br/><br/>
+            <footer>
+                <p>Copyright ⓒ {year}</p>
+            </footer>
         </div>
     );
 }
